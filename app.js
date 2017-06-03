a366e43b57c3e6ad1357fb4e1c2beea43d2378ad
Tsubasa Nakayama <app.nakayama@gmail.com>
2017-06-03 23:13:02 +0900

    Ajaxの実装

--- app.js
+++ app.js
@@ -1,21 +1,17 @@
 var express = require('express');
 var ejs = require('ejs');
 var app = express();
-var session = require('express-session');
 
-app.engine('ejs', ejs.renderFile);
-app.use(session( {
-    secret: 'hoge',
-    resave: true,
-    saveUninitialized: true,
-}));
+var msg = '';
 
 app.get('/', function(req, res) {
-    var count = req.session.count == undefined ? 0 : req.session.count;
-    count++;
-    req.session.count = count;
-    res.render('temp.ejs', {
-        count: count
+    res.render('temp.ejs', {});
+});
+
+app.post('/ajax', function(req, res) {
+    msg += 'hoge';
+    res.json ({
+        msg: msg
     });
 });
 
